b92adf5b6ba3c3a9987653534df19786caf993e2
dnqxj <dnqxj@icloud.com>
2021-10-02 15:47:24 +0800

    1

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ DEBUG=True
 
 app = {
     'base_url': 'https://www.baidu.com',
-    # 手机端: iPhone6/7/8
+    # 手机端: device_name: iPhone 6/7/8
     'device_name': '',
     'chrome_path': r'D:\Python\chromedriver.exe',
     'window_width': 1000,
